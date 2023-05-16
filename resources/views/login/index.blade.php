39882bd1b92c4d61e57f1f3c01db899405f62fde
ekawahyubudisantoso <ekawahyubudisantoso22@gmail.com>
2023-05-16 08:26:21 +0700

    Belajar User Registration

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,6 +3,15 @@
 @section('container')
     <div class="row justify-content-center">
         <div class="col-md-5">
+
+            @if (session()->has('success'))
+                
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
             <main class="form-signin w-100 m-auto">
                 <h1 class="h3 mb-3 fw-normal">Please login</h1>
                 <form>
